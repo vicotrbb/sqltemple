042a666c2199b8f257c8fb5ac03275a7c5996ff6
vicotrbb <victor.bona@hotmail.com>
2025-12-12 20:16:38 -0300

    Fixed OG images

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -1,14 +1,14 @@
-import { defineConfig } from 'astro/config';
-import react from '@astrojs/react';
-import tailwind from '@astrojs/tailwind';
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from "astro/config";
+import react from "@astrojs/react";
+import tailwind from "@astrojs/tailwind";
+import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
-  site: 'https://sqltemple.dev',
+  site: "https://sqltemple.dev",
   integrations: [react(), tailwind(), sitemap()],
   markdown: {
     shikiConfig: {
-      theme: 'github-dark',
+      theme: "github-dark",
       wrap: true,
     },
   },
